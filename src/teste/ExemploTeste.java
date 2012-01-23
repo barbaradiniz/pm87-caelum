c4f314bf74612672885f0d796e00a415444c55b3
barbara <bdsp.pe@gmail.com>
2012-01-23 10:55:45 -0200

    primeiro commit do projeto

---
+++ src/teste/ExemploTeste.java
@@ -0,0 +1,13 @@
+package teste;
+
+public class ExemploTeste {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Exemplo de teste de uso do git");
+
+	}
+
+}
